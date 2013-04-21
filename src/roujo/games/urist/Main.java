2a0940a3adcde4e6f6b38b68d75ed69f09774e2d
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2013-04-21 01:33:10 -0400

    And here we go: Characters drawn across the window. Looking good! =D

--- src/roujo/games/urist/Main.java
+++ src/roujo/games/urist/Main.java
@@ -1,5 +1,11 @@
 package roujo.games.urist;
 
+import roujo.games.urist.data.GameConfig;
+import roujo.games.urist.ui.Character;
+import roujo.games.urist.ui.Drawer;
+import roujo.games.urist.ui.GraphicsHandler;
+import roujo.lib.gui.windows.GraphicWindow;
+
 public class Main {
 
 	/**
@@ -7,7 +13,23 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-
+		
+		GameConfig.getInstance().load();
+		
+		GraphicsHandler gh = GraphicsHandler.getInstance();
+		gh.init();
+		GraphicWindow window = gh.getGameWindow();
+		
+		Drawer d = gh.getDrawer();
+		d.init();
+		int i = 0;
+		for(Character c : Character.values())
+			d.draw(c, ++i * 150, i * 150);
+		d.commit();
+		
+		while(true){
+			window.draw();
+		}
 	}
 
 }
