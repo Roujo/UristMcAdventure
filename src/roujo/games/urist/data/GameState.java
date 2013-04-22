93a76248dc9cf96a95dfff1bc04d56040b199be3
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2013-04-21 21:34:58 -0400

    Nevermind, that much use of generics wasn't exactly healthy.

--- src/roujo/games/urist/data/GameState.java
+++ src/roujo/games/urist/data/GameState.java
@@ -1,10 +1,5 @@
 package roujo.games.urist.data;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import roujo.games.urist.entities.Entity;
-import roujo.games.urist.entities.EntityType;
 import roujo.games.urist.entities.Player;
 import roujo.games.urist.entities.util.EntityContainer;
 
@@ -15,14 +10,13 @@ public class GameState {
 		return INSTANCE;
 	}
 	
-	private Map<EntityType, EntityContainer<? extends Entity>> containers;
+	private EntityContainer<Player> players;
 	
 	private GameState() {
-		containers = new HashMap<EntityType, EntityContainer<? extends Entity>>();
-		containers.put(EntityType.Player, new EntityContainer<Player>());
+		players = new EntityContainer<Player>();
 	}
 	
-	public EntityContainer<? extends Entity> getContainer(EntityType type) {
-		return containers.get(type);
+	public EntityContainer<Player> getPlayerContainer() {
+		return players;
 	}
 }
