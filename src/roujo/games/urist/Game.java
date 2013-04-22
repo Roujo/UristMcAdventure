02fee75631a4f6d0392924c817858d5713c675a1
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2013-04-21 21:43:00 -0400

    Added Master Draw Loop. Draws all visible GameState entities. This *might* be too clever for my own good. We'll see. =P

--- src/roujo/games/urist/Game.java
+++ src/roujo/games/urist/Game.java
@@ -4,7 +4,10 @@ import java.awt.KeyboardFocusManager;
 import java.awt.event.KeyEvent;
 
 import roujo.games.urist.data.GameConfig;
+import roujo.games.urist.data.GameState;
+import roujo.games.urist.entities.Entity;
 import roujo.games.urist.entities.Player;
+import roujo.games.urist.entities.util.EntityContainer;
 import roujo.games.urist.input.KeyboardInput;
 import roujo.games.urist.ui.Drawer;
 import roujo.games.urist.ui.GraphicsHandler;
@@ -14,6 +17,7 @@ public class Game {
 	private GraphicsHandler graphicsHandler;
 	private KeyboardInput keyboardInput;
 	private boolean running;
+	private GameState gameState;
 	private Player p1, p2;
 
 	public Game() {
@@ -28,9 +32,15 @@ public class Game {
 		KeyboardFocusManager manager = KeyboardFocusManager.getCurrentKeyboardFocusManager();
 		manager.addKeyEventDispatcher(keyboardInput);
 		
-		// TODO: Move Entities to some GameState class
 		p1 = new Player(Character.P1, 100, 100);
+		p1.setVisible(true);
 		p2 = new Player(Character.P2, 700, 700);
+		p2.setVisible(true);
+		
+		gameState = GameState.getInstance();
+		EntityContainer<Player> players = gameState.getPlayerContainer();
+		players.add(p1);
+		players.add(p2);
 		
 		running = true;
 		while(running){
@@ -70,8 +80,13 @@ public class Game {
 	private void drawScreen() {
 		Drawer drawer = graphicsHandler.getDrawer();
 		drawer.init();
-		drawer.draw(p1);
-		drawer.draw(p2);
+		
+		// Here we go: Master Draw Loop
+		for(EntityContainer<? extends Entity> entityContainer : gameState.getEntityContainerList())
+			for(Entity entity : entityContainer.getAll())
+				if(entity.isVisible())
+					drawer.draw(entity);
+		
 		drawer.commit();
 		graphicsHandler.getGameWindow().draw();
 	}
