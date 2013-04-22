2f9fb551b46c02d37b74a5d20c752dbd53000672
Jonathan Rouillard <jonathan.rouillard@gmail.com>
2013-04-21 21:27:39 -0400

    Getting interesting: Added Class to EntityType.

--- src/roujo/games/urist/entities/EntityType.java
+++ src/roujo/games/urist/entities/EntityType.java
@@ -1,5 +1,15 @@
 package roujo.games.urist.entities;
 
 public enum EntityType {
-	Player;
+	Player(Player.class);
+	
+	private Class<? extends Entity> entityClass;
+	
+	private EntityType(Class<? extends Entity> entityClass) {
+		this.entityClass = entityClass;
+	}
+	
+	public Class<? extends Entity> getEntityClass() {
+		return entityClass;
+	}
 }
